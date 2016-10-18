c2f0b5b459a3e9593bb522170df9b15f4b8ed972
andsel <selva.andre@gmail.com>
2016-10-19 00:24:44 +0200

    Minor, just extracted some factory methods for bootstrap data

--- web/src/main/java/org/dna/web/infrastructure/BootstrapData.java
+++ web/src/main/java/org/dna/web/infrastructure/BootstrapData.java
@@ -22,6 +22,18 @@ public class BootstrapData implements ApplicationListener<ContextRefreshedEvent>
 
     private static Logger LOG = LoggerFactory.getLogger(BootstrapData.class);
 
+    private final String[] abilities = {"Arieggiatura prato", "Potatura siepi e piccoli cespugli",
+            "Piantumazione siepi e piccoli alberi", "Potatura di mantenimento", "Attrezzatura propria",
+            "Smaltimento sfalci", "Pacciamatura", "Preparazione e semina del prato"};
+
+    private final String[] competences = {"Precisione", "Competenza", "Puntualità", "Flessibilità orario"};
+
+    private final Review[] reviews = {new Review("Lavoratore attento, puntuale. E' sempre stato disponibile anche lavori" +
+            "per cui non era specializzato portandoli a termine bene", 4.5),
+            new Review("Il lavoro svolto non era all'altezza delle aspettative, non molto cordiale", 2.0),
+            new Review("Lavoratore onesto senza lode e senza infamia, ma il suo lo sa fare", 3.8)
+    };
+
     @Autowired
     private TaskerRepository taskerRepository;
 
@@ -30,22 +42,11 @@ public class BootstrapData implements ApplicationListener<ContextRefreshedEvent>
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
+
         LOG.debug("Populating bootstrap data");
         //stores 12 greenkeepers
         String[] greenKeepers = {"Mario", "Andrea", "Alessandro", "Marco", "Antonio", "Carlo", "Davide", "Italo",
                 "Enrico", "Federico", "Giacomo", "Luigi", "Mimmo"};
-
-        String[] abilities = {"Arieggiatura prato", "Potatura siepi e piccoli cespugli", "Piantumazione siepi e piccoli alberi",
-                "Potatura di mantenimento", "Attrezzatura propria", "Smaltimento sfalci", "Pacciamatura",
-                "Preparazione e semina del prato"};
-
-        String[] competences = {"Precisione", "Competenza", "Puntualità", "Flessibilità orario"};
-
-        Review[] reviews = {new Review("Lavoratore attento, puntuale. E' sempre stato disponibile anche lavori" +
-                "per cui non era specializzato portandoli a termine bene", 4.5),
-                new Review("Il lavoro svolto non era all'altezza delle aspettative, non molto cordiale", 2.0),
-                new Review("Lavoratore onesto senza lode e senza infamia, ma il suo lo sa fare", 3.8)
-        };
 
         for (String taskerName : greenKeepers) {
             Tasker greenKeeper = new Tasker(taskerName, "Gardening", "Trento", "IT");
@@ -55,27 +56,9 @@ public class BootstrapData implements ApplicationListener<ContextRefreshedEvent>
             greenKeeper.addSkill(SkillType.GREENKEEPING);
             greenKeeper.setFeedback(new FeedbackSummary((int)round(random() * 100), random() * 5));
 
-            //Add random abilities
-            Set<Ability> abilitiesForTasker = new HashSet<>();
-            while (abilitiesForTasker.size() < 6) {
-                abilitiesForTasker.add(new Ability(abilities[(int)(random() * abilities.length)]));
-            }
-            abilitiesForTasker.forEach(greenKeeper::addAbility);
-
-            //Add random competences
-            Set<Competence> competencesForTasker = new HashSet<>();
-            while (competencesForTasker.size() < 4) {
-                int randomCompetenceIdx = (int)(random() * competences.length);
-                int competenceVotes = (int)(random() * 20);
-                competencesForTasker.add(new Competence(competences[randomCompetenceIdx], competenceVotes));
-            }
-            competencesForTasker.forEach(greenKeeper::addCompetence);
-
-            //Add some reviews
-            int numReviews = (int)(random() * reviews.length);
-            for (int i=0; i<numReviews; i++) {
-                greenKeeper.addReview(reviews[(int)(random() * reviews.length)]);
-            }
+            addSixRandomAbilities(greenKeeper);
+            addFourRandomCompetences(greenKeeper);
+            addSomeRandomReviews(greenKeeper);
 
             taskerRepository.save(greenKeeper);
         }
@@ -94,5 +77,31 @@ public class BootstrapData implements ApplicationListener<ContextRefreshedEvent>
         bidderRepository.save(bidderPablo);
 
         LOG.debug("Finished populate bootstrap data");
+    }
+
+    private void addSomeRandomReviews(Tasker greenKeeper) {
+        //Add some reviews
+        int numReviews = (int)(random() * reviews.length);
+        for (int i=0; i<numReviews; i++) {
+            greenKeeper.addReview(reviews[(int)(random() * reviews.length)]);
+        }
+    }
+
+    private void addFourRandomCompetences(Tasker greenKeeper) {
+        Set<Competence> competencesForTasker = new HashSet<>();
+        while (competencesForTasker.size() < 4) {
+            int randomCompetenceIdx = (int)(random() * competences.length);
+            int competenceVotes = (int)(random() * 20);
+            competencesForTasker.add(new Competence(competences[randomCompetenceIdx], competenceVotes));
+        }
+        competencesForTasker.forEach(greenKeeper::addCompetence);
+    }
+
+    private void addSixRandomAbilities(Tasker greenKeeper) {
+        Set<Ability> abilitiesForTasker = new HashSet<>();
+        while (abilitiesForTasker.size() < 6) {
+            abilitiesForTasker.add(new Ability(abilities[(int)(random() * abilities.length)]));
+        }
+        abilitiesForTasker.forEach(greenKeeper::addAbility);
     }
 }
